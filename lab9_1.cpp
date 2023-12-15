d883339ccba995e6c4b98dba26cc490f865007dc
rutchanon0878 <rutchanon_inkam@cmu.ac.th>
2023-12-15 16:41:15 +0700

    finish

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 int main(){	
+	double l,py,cp;
 	cout << "Enter initial loan: ";
+	cin >> l;
 	cout << "Enter interest rate per year (%): ";
+	cin >> py;
 	cout << "Enter amount you can pay per year: ";
-
+	cin >> cp;
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -17,17 +20,26 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	double mt = l;
+	double N ;
+	int i=1;
+	do{
+	double it = (py/100)*mt;
+	double t = mt + it;
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	cout << setw(13) << left << i;
+		i++; 
+	cout << setw(13) << left << mt;
+	cout << setw(13) << left << it;
+	cout << setw(13) << left << t;
+	if (t < cp) cp = t;
+	cout << setw(13) << left << cp;
+	N = t-cp;
+	cout << setw(13) << left << N;
+	mt = N;
+	cout << "\n";
+	}while(N > 0);
 	return 0;
 }
